4473f3556fac74d28e7181ad25bb2b46bbfe1dd6
jreyesr <jreyesr@users.noreply.github.com>
2018-10-30 16:22:18 -0500

    Aplicar InterfaceSegregationPrinciple

---
+++ solid/src/_3isp/bien/EntrenamientoPokemonBasico.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _3isp.bien;
+
+
+/**
+ * 
+ * @author Palacios
+ */
+
+
+public interface EntrenamientoPokemonBasico {
+    void aprenderAtaqueCorte();   //todo pokemon tiene esta habilidad
+    
+    
+}
